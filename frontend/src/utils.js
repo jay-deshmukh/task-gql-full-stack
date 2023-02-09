79e18bfdbbc338add25e68bdd60cec052bb084b9
fatmakhv <fatmaba@gmail.com>
2023-02-09 14:31:05 +0000

    Object manipulation test passed.

--- frontend/src/utils.js
+++ frontend/src/utils.js
@@ -3,6 +3,10 @@
  */
 
 export const copyObject = (obj) => {
+  /* Fatma: Problem 2.1. - Commented out - shallow copy and added deep copy below
   const copyObj = {...obj};
+  (missing package: npm install --save-dev @babel/preset-env)
+   */
+  const copyObj = JSON.parse(JSON.stringify(obj)); // deep copy
   return copyObj;
 }
