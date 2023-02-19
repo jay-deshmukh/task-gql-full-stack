6dceb386d25b56b9b123ca37c6bcb4780641a585
Mbaoma <mmaryraphaella@gmail.com>
2023-02-19 19:39:24 +0100

    add frontend solution

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,48 @@
-import './App.css';
+import './App.css'
+import axios from 'axios'
+import { QueryClient, QueryClientProvider, useQuery } from 'react-query'
 
-function App() {
+const queryClient = new QueryClient()
+
+const endpoint = 'http://localhost:8085/query'
+const HEROES_QUERY = `
+{ heroes { name ...on Human { hasLightsaber } ...on Droid { primaryFunction } } }
+`
+
+export default function App() {
   return (
-    <div>
-      {/* Your Code Here */}
-    </div>
-  );
+    <QueryClientProvider client={queryClient}>
+      <Example />
+    </QueryClientProvider>
+  )
 }
+function Example() {
+  const { data, isLoading, error } = useQuery('heroes', () => {
+    return axios({
+      url: endpoint,
+      method: 'post',
+      data: {
+        query: HEROES_QUERY
+      }
+    }).then((result) => result.data.data)
+  })
 
-export default App;
+  if (isLoading) return 'Loading...'
+  if (error) return <pre>{error.message}</pre>
+
+  return (
+    <div className='main'>
+      <div className='main-content'>
+        <div className='box-wrap'>
+          {data.heroes.map((hero) => (
+            <div className='box' key={hero.name}>
+              {hero.name}
+            </div>
+          ))}
+          <div className='box'>.....</div>
+        </div>
+        <div className='main-box'>GraphiQL</div>
+      </div>
+    </div>
+  )
+}
